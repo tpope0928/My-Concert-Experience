5b28eba3f43b9ebc6c98339b92905b2ddc2f703e
Thomas_Pope <tpope0928@gmail.com>
2019-10-29 21:55:42 -0400

    user model work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :experiences
-    has_many :concerts, through: :experiences
+    has_many :exp_concerts
+
+    has_many :exp_concerts, through: :experiences, source: :concert
 end
