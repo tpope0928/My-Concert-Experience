1d63c4970ab1dc0326d5025d9f95adc3cef2fea1
tpope19 <40743479+tpope19@users.noreply.github.com>
2019-10-29 22:32:23 -0400

    signup view and controller logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
   
   resources :experiences
   resources :concerts
