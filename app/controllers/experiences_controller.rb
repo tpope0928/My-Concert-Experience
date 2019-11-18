062f71a3565279ca103c54d8a4d8aa76549b4560
Thomas_Pope <tpope0928@gmail.com>
2019-11-17 19:19:25 -0500

    startd working on adding experiences to concerts

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,2 +1,37 @@
 class ExperiencesController < ApplicationController
-end
+    def new
+        if @concert = Concert.find_by_id(params[:concert_id])
+          @experience = @concert.experiences.build
+        else
+          @experience = Experience.new
+        end
+      end
+    
+      def create
+        @experience = current_user.experiences.build(experience_params)
+        if @experience.save
+          redirect_to experience_path(@experience)
+        else
+          render :new
+        end
+      end
+    
+      def show
+        @experience = Experience.find_by_id(params[:id])
+      end
+    
+      def index
+        if @concert = Concert.find_by_id(params[:concert_id])
+          @experiences = @concert.experiences
+        else
+          @experiences = Experience.all
+        end
+      end
+    
+      private
+    
+      def experience_params
+          params.require(:experience).permit(:concert_id, :content, :rating)
+      end
+
+    end
