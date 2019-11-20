12ebc7a51820187bf455cf847a0ba226faef807d
Thomas_Pope <tpope0928@gmail.com>
2019-11-19 22:20:07 -0500

    experience adding work. Need to make experience show page and connect to concert

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   
   resources :experiences
-  resources :concerts
+  resources :concerts do
+    resources :experiences, only: [:new, :index]
+  end
   resources :artists
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
